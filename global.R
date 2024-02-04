c55e5c9ac1833f5bba5c1c10fffbc8cdca0a3d1f
Peter Menzies <petermenzies@bren.ucsb.edu>
2024-02-04 12:10:33 -0800

    remove unused imports

--- global.R
+++ global.R
@@ -1,12 +1,9 @@
 
 library(shiny)
 library(shinydashboard)
-library(shinyWidgets)
 library(tidyverse)
-library(here)
-library(knitr)
 library(leaflet)
 library(sf)
 library(shinycssloaders)
 
-points <- read_rds(here("data/temp/points.RDS"))
+points <- read_rds("data/temp/points.RDS")
